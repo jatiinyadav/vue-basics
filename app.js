c41243089d1bea8589adaeabe0e8208978b466bd
jatiinyadav <jatin27yadav@gmail.com>
2022-10-02 23:39:39 +0530

    Basics

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    // data, functions
+    // template: '<h1>Hello Wolrdd</h1>'
+    data() {
+        return {
+            title: "Jatin Yadav",
+            age: 21,
+            area: "New Delhi"
+        }
+    }
+})
+
+app.mount("#app")
