5543fd81ec283b23ade41bda624e5a6f9d9ea6e2
jatiinyadav <jatin27yadav@gmail.com>
2022-10-25 15:49:33 +0530

    typed.js

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const app = Vue.createApp({
-    // data, functions
-    // template: '<h1>Hello Wolrdd</h1>'
-    data() {
-        return {
-            title: "Vue Basics",
-            heading: "",
-            headingTrim:"",
-            headingLazy: "",
-            itemArray: [],
-            headingNumber: "",
-            items: [{id: 1, label :"Item1"}, {id: 2, label: "Item2"}, {id: 3, label: "Item3"}],
-            awesome: true,
-            count: 0,
-            myObject: {
-                Title: 'How to do lists in Vue',
-                Author: 'Jatin Yadav',
-                PublishedAt: '2022-10-23'
-              }
-        }
-    }
-})
+  // data, functions
+  // template: '<h1>Hello Wolrdd</h1>'
+  data() {
+    return {
+      title: "Vue Basics",
+      heading: "",
+      headingTrim: "",
+      headingLazy: "",
+      itemArray: [],
+      headingNumber: "",
+      items: [
+        { id: 1, label: "Item1" },
+        { id: 2, label: "Item2" },
+        { id: 3, label: "Item3" },
+      ],
+      awesome: true,
+      count: 0,
+      myObject: {
+        Title: "How to do lists in Vue",
+        Author: "Jatin Yadav",
+        PublishedAt: "2022-10-23",
+      },
+      typed: new Typed(".mouse", {
+        strings: [
+          "Welcome to Vue Basics",
+          "Create your own PR's",
+          "Star us on Github ⭐",
+        ],
+        typeSpeed: 40,
+        backSpeed: 40,
+        fadeOut: true,
+        loop: true,
+      }),
+    };
+  },
+});
 
-app.mount("#app")
+app.mount("#app");
